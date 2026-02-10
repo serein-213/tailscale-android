4064e9e68a4e37035b6b4ff742b7108d8550a285
serein-213 <revdd@vip.qq.com>
2026-02-10 16:05:03 +0800

    feat: add custom control server settings and admin console URL support

--- android/src/main/java/com/tailscale/ipn/App.kt
+++ android/src/main/java/com/tailscale/ipn/App.kt
@@ -114,6 +114,7 @@ class App : UninitializedApp(), libtailscale.AppContext, ViewModelStoreOwner {
     super.onCreate()
     TSLog.init(this)
     com.tailscale.ipn.ui.theme.ThemeConfig.init(this)
+    com.tailscale.ipn.ui.util.ServerConfig.init(this)
     appInstance = this
     setUnprotectedInstance(this)
     mdmChangeReceiver = MDMSettingsChangedReceiver()
--- android/src/main/java/com/tailscale/ipn/MainActivity.kt
+++ android/src/main/java/com/tailscale/ipn/MainActivity.kt
@@ -312,6 +312,9 @@ class MainActivity : ComponentActivity() {
                           onNavigateToUserSwitcher = { navController.navigate("userSwitcher") },
                           onNavigateToPermissions = { navController.navigate("permissions") },
                           onNavigateToThemeSettings = { navController.navigate("themeSettings") },
+                          onNavigateToCustomControl = {
+                            navController.navigate("loginWithCustomControl")
+                          },
                           onBackToSettings = backTo("settings"),
                           onNavigateBackHome = backTo("main"))
                   val exitNodePickerNav =
@@ -403,7 +406,7 @@ class MainActivity : ComponentActivity() {
                   }
                   composable("loginWithCustomControl") {
                     LoginWithCustomControlURLView(
-                        onNavigateHome = backTo("main"), backTo("userSwitcher"))
+                        onNavigateHome = backTo("main"), backToSettings = { navController.popBackStack() })
                   }
                 }
             if (isIntroScreenViewedSet()) {
